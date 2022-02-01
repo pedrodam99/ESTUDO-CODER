e0dfd9a0cd9b489974fbc244c22fe44c5090be1f
Pedro - PC ANTIGO <pedrodam99@gmail.com>
2022-02-01 20:27:47 -0300

    Tipos de Valores

---
+++ javascript/fundamentos/tipos-de-valores.js
@@ -0,0 +1,28 @@
+//tipo number 
+let n1 = 123
+let n2 = Number('123')
+
+
+//tipo string (valores alfanumericos)
+let s1 = ('Hello World')
+let s2 = String('Hello World')
+
+//tipo boolean(true or false)
+let b1 = true
+
+//casos que dá true
+//números inteiros tirando o 0
+//string com um espaço ou cheia = true
+//array vazio = true 
+//objeto vazio = true
+//infinity = true
+//atribuição = true
+
+let b2 = false
+// casos que da false 
+// number 0 = false
+// string vazia = false
+// NaN = false
+// null = false
+// undefined = false
+
