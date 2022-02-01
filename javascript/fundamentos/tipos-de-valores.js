968cfdd945d0da8f6001a95ed6d6b1aeb885af2d
Pedro - PC ANTIGO <pedrodam99@gmail.com>
2022-02-01 20:50:14 -0300

    Tipos de Valores

--- javascript/fundamentos/tipos-de-valores.js
+++ javascript/fundamentos/tipos-de-valores.js
@@ -1,13 +1,13 @@
-//tipo number 
+//TIPO NUMBER
 let n1 = 123
 let n2 = Number('123')
 
 
-//tipo string (valores alfanumericos)
+//TIPO STRING (VALORES ALFANUMERICOS)
 let s1 = ('Hello World')
 let s2 = String('Hello World')
 
-//tipo boolean(true or false)
+//TIPO BOOLEAN (APENAS TRUE OR FALSE)
 let b1 = true
 
 //casos que dá true
@@ -18,11 +18,43 @@ let b1 = true
 //infinity = true
 //atribuição = true
 
-let b2 = false
+let b2 = Boolean(false)
 // casos que da false 
 // number 0 = false
 // string vazia = false
 // NaN = false
 // null = false
 // undefined = false
+
+//TIPO ARRAY
+//array é uma estrutura unidimensional e indexada 
+//no js ela é heterogênea, ou seja, aceita diversos tipos de dados no mesmo array
+//não tem tamanho fixo
+//seu indice começa no [0]
+
+const array1 = [1, 'Teste']
+
+//TIPO OBJECT
+//coleção de chave e valor
+//o valor pode ser variaveis, funções e até mesmos outros objetos
+
+const Pessoa = {
+    nome: 'Paulo Cezar',
+    cpf: 1234343232
+}
+
+//TIPO FUNCTION
+//a function é um bloco de código que pode ou não retornar um valor 
+//pode ou não utilizar parametros
+
+//sem retorno
+function soma(a, b) {
+	console.log(a + b)
+}
+
+//com retorno
+function newSoma (n1, n2) {
+	return n1 + n2
+}
+const result = newSoma (5, 7)
 
