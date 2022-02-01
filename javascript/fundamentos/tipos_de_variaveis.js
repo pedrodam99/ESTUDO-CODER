c8c4ec8ed8c5267742b1c5edb094edf4425558da
Pedro - PC ANTIGO <pedrodam99@gmail.com>
2022-02-01 20:08:06 -0300

    Tipos de Variavel

---
+++ javascript/fundamentos/tipos_de_variaveis.js
@@ -0,0 +1,13 @@
+//caiu em desuso por problemas de escopo;
+//pode ser sobrescrita
+var a = 1
+
+// o let substitui o var, corrigindo problemas de escopo
+//também pode ser sobrescrita 
+let b = 2 
+
+//const é a mais recomendada, já que tem limitação de escopo
+//pode ser sobrescrita
+const c = 3
+
+
