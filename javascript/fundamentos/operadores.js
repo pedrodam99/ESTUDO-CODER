0beea47b6dbbd6e0b2aa17623844b6f228e8b100
Pedro - PC ANTIGO <pedrodam99@gmail.com>
2022-02-02 16:22:19 -0300

    Tipos de Operadores

---
+++ javascript/fundamentos/operadores.js
@@ -0,0 +1,43 @@
+//OPERADORES ARITMÉTICOS
+const soma = a + b 
+const subtracao = a - b 
+const multiplicacao = a * b
+const divisao = a / b
+const modulo = a % 2
+const exponenciacao = a ** b  
+
+//OPERADORES DE ATRIBUIÇÃO
+b += 10 // b = b + 10 <= soma
+b -= 4 // b = b - 4 <= subtração 
+b *= 2 // b = b * 2 <= multiplicação
+b /= 2 // b = b / 2 <= divisão
+b %= 2 // b = b % 2 <= módulo
+b **= 2 // b = b ** 2 <= exponenciacao
+
+//OPERADORES UNARIOS
+//atuam em cima de apenas uma variável
+
+num++
+num--
+++num  
+--num
+
+//OPERADORES TERNARIOS
+//atuam com tres operandos
+//especie de if / else
+nota >= 7 ? 'Aprovado' : 'Reprovado'
+
+//OPERADORES RELACIONAIS
+// resultado sempre retornará false ou true
+//Temos os classicos >, <, >=, <=
+//Temos também ==, ===, !=, !==
+
+//OPERADORES LÓGICOS
+// OU (||) apenas uma condição precisa ser TRUE
+// E (&&) todas as condições precisam ser TRUE
+// NEGAÇÃO LÓGICA (!) inverte o resultado
+
+
+
+
+
