a3be1f669ec5dc9c453ad7a8364da9845e4b5ed4
Pedro - PC ANTIGO <pedrodam99@gmail.com>
2022-02-11 10:51:55 -0300

    Funçâo Construtora X Factory function X Classe

---
+++ javascript/function/class_factory_construtora.js
@@ -0,0 +1,32 @@
+//! Criando uma Pessoa usando Classe
+
+class Pessoa1 {
+    constructor(nome){
+        this.nome = nome
+    }
+    falar(){
+        console.log("Meu nome é " + this.nome)
+    }
+}
+const p1 = new Pessoa1('João')
+p1.falar()
+
+//! Criando uma Pessoa usando Factory
+//* Usando a factory, o this não muda, diferente da classe que o this pode variar
+
+const Pessoa2 = nome => {
+    return {
+        falar: () => console.log('Meu nome é ' + nome)
+    }
+}
+const p2 = Pessoa2('Pedro')
+p2.falar()
+
+//! Transformando Classe em função construtora
+
+function Pessoa3(nome){
+    this.nome = nome
+    this.falar = function(){ console.log('Meu nome é '+ this.nome)}
+}
+const p3 = new Pessoa3('Paulo')
+p3.falar()
