02d25483edd072bd5a7c35062f113d89970d2b6f
Pedro - PC ANTIGO <pedrodam99@gmail.com>
2022-02-21 18:07:56 -0300

    Add Concat

---
+++ javascript/array/concat.js
@@ -0,0 +1,9 @@
+//! CONCAT
+//* Serve para juntar arrays
+
+const a1 = ['segunda', 'terça', 'quarta']
+const a2 = ['quinta', 'sexta']
+const a3 = ['sabado', 'domingo']
+
+const semana = a1.concat(a2, a3)
+console.log(semana)
